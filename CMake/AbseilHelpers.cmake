67c2686dddfe533ad313803b877bd146178fd375
openvela-robot <openvela-robot@xiaomi.com>
2022-07-12 09:12:37 +0800

    Merge pull request #1216 from brjsp:master
    PiperOrigin-RevId: 460478343
    Change-Id: I58e2e1fb7b1776f37b1254ba5fd74d56b44cb4d0

--- CMake/AbseilHelpers.cmake
+++ CMake/AbseilHelpers.cmake
@@ -167,7 +167,8 @@ function(absl_cc_library)
         elseif(${cflag} MATCHES "^(-W|/w[1234eo])")
           # Don't impose our warnings on others.
         elseif(${cflag} MATCHES "^-m")
-          # Don't impose CPU instruction requirements on others, as the code performs feature detection on runtime.
+          # Don't impose CPU instruction requirements on others, as
+          # the code performs feature detection on runtime.
         else()
           set(PC_CFLAGS "${PC_CFLAGS} ${cflag}")
         endif()
