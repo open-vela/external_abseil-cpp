60dfba7930a3897340af6524cf8705f83884a50f
liujinye <liujinye@xiaomi.com>
2023-02-21 12:15:56 +0800

    Merge pull request #1401 from AtariDreams:apple
    PiperOrigin-RevId: 511270860
    Change-Id: Ib7de8c72505bf141d450f950074a27f9f4f1613c

--- absl/container/internal/raw_hash_set.h
+++ absl/container/internal/raw_hash_set.h
@@ -1889,8 +1889,11 @@ class raw_hash_set {
   //   const char* p = "hello";
   //   s.insert(p);
   //
+  // TODO(romanp): Once we stop supporting gcc 5.1 and below, replace
+  // RequiresInsertable<T> with RequiresInsertable<const T&>.
+  // We are hitting this bug: https://godbolt.org/g/1Vht4f.
   template <
-      class T, RequiresInsertable<const T&> = 0,
+      class T, RequiresInsertable<T> = 0,
       typename std::enable_if<IsDecomposable<const T&>::value, int>::type = 0>
   std::pair<iterator, bool> insert(const T& value) {
     return emplace(value);
@@ -1914,8 +1917,11 @@ class raw_hash_set {
     return insert(std::forward<T>(value)).first;
   }
 
+  // TODO(romanp): Once we stop supporting gcc 5.1 and below, replace
+  // RequiresInsertable<T> with RequiresInsertable<const T&>.
+  // We are hitting this bug: https://godbolt.org/g/1Vht4f.
   template <
-      class T, RequiresInsertable<const T&> = 0,
+      class T, RequiresInsertable<T> = 0,
       typename std::enable_if<IsDecomposable<const T&>::value, int>::type = 0>
   iterator insert(const_iterator, const T& value) {
     return insert(value).first;
